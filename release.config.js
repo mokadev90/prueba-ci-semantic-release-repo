174f5b92084a8ccd961273bc368884b145a96178
Moka <romeramatias@gmail.com>
2023-12-20 11:54:18 -0300

    ci

--- release.config.js
+++ release.config.js
@@ -1,34 +1,6 @@
 const config = {
   branches: 'master',
-  repositoryUrl: 'https://github.com/thecodinganalyst/semantic-java',
-  debug: 'false',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-        changelogTitle: '# Semantic Versioning Changelog',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md'],
-      },
-    ],
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          {
-            path: 'release/**',
-          },
-        ],
-      },
-    ],
-  ],
+  verifyConditions: ['@semantic-release/github'],
 };
 
 module.exports = config;
